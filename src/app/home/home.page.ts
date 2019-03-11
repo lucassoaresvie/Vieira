91fe283f796281682b26b89149f97a9efe1d8c1e
lucassoaresvie <lucassoaresvie@gmail.com>
2019-03-11 12:13:20 -0300

    segunda_versao

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  nome : string = "Musica";
+
+  clicar() : void {
+    alert("🎶 🎶 🎶 🎶 🎶 🎶 🎶 🎶 🎶 🎶 🎶 🎶");
+  }
 
 }
